d1f68eef270c08a4ae145cbede3af11290f8adc7
marc18x <38349590+Marc18x@users.noreply.github.com>
2018-05-22 17:55:48 +0800

    Add files via upload

---
+++ fileRead.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define MAXSIZE 100
+
+int main()
+{
+    FILE *fp;
+    char a[MAXSIZE];
+    int b,c[MAXSIZE],len=0,i;
+    //ÎÄ¼þ¶ÁÈ¡
+
+    if((fp = fopen("z:\\number.txt","r"))==NULL)
+    {
+        printf("File doesn't exist.");
+        exit(1);
+    }
+
+    fscanf(fp,"%d",&b);
+    while(!feof(fp)){
+        fscanf(fp,"%d",&b);
+        c[len++]=b;
+}
+    fclose(fp);
+
+        if((fp = fopen("z:\\write.txt","w"))==NULL)
+    {
+        printf("File doesn't exist.");
+        exit(1);
+    }
+    for(i=0;i<len;i++){
+        fprintf(fp,"%d\t",c[i]);
+    }
+      fclose(fp);
+      printf("success!");
+    return 0;
+}
